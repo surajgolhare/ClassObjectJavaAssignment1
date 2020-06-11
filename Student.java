031375fd1fc0a5d142439a3108b9523449b508eb
unknown <you@example.com>
2020-06-11 10:53:48 +0530

    Student information

---
+++ Student.java
@@ -0,0 +1,12 @@
+class Student{
+  String name;
+  int roll_no;
+
+  public static void main(String[] args){
+    Student s1 = new Student();
+    s1.name = "John";
+    s1.roll_no = 2;
+    System.out.println("Name is "+s1.name+" and roll number is "+s1.roll_no);
+  }
+}
+
